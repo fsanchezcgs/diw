6d4d64119eda9f79a137f5481a0df069d0cea23b
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-01-25 21:15:49 +0100

    25-01-2024

--- UD6_Activity4/script.js
+++ UD6_Activity4/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+  let id = 0;
+  let red = 0;
+  let blue = 0;
+  let green = 0;
+  show();
   $(".container").droppable();
+  function show() {
+    $("#redCounter").text(`${red}`);
+    $("#blueCounter").text(`${blue}`);
+    $("#greenCounter").text(`${green}`);
+  };
   $("#generate").on("click", () =>{
+    id++;
     let colorNumber = Math.floor(Math.random() * (4 - 1) + 1);
     let xNumber = Math.floor(Math.random() * (900 - 1) + 1);
     let yNumber = Math.floor(Math.random() * (900 - 1) + 1);
@@ -13,10 +24,17 @@ $(document).ready(function() {
       color = "green1";
     }
 
-    $("main").after(`<div class="${color} drag" style="left: ${xNumber}px; top: ${yNumber}px;"></div>`);
+    $("main").after(`<div id="${id}" class="${color} drag" style="left: ${xNumber}px; top: ${yNumber}px;"></div>`);
     $(".drag").draggable();
   });
   $(".container").on("drop", (e) => {
-    console.log(e.target);
+    if($(e.target).hasClass("red") && $(e.originalEvent.target).hasClass("red1")) {
+      red++;
+    } else if($(e.target).hasClass("blue") && $(e.originalEvent.target).hasClass("blue1")) {
+      blue++;
+    } else if($(e.target).hasClass("green") && $(e.originalEvent.target).hasClass("green1")) {
+      green++;
+    }
+    show();
   });
 });

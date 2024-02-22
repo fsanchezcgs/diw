f4e95a3234ff92e9f23de86e9de36354105bbf06
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2024-02-22 21:16:43 +0100

    almost finished

---
+++ UD7_Activity3/html/posts/Post.js
@@ -0,0 +1,24 @@
+export default {
+  name: "Post",
+  props: ["title", "content", "author", "creationDate", "image"],
+  methods: {
+    editPost: function (index) {
+      this.$emit("editPost", index);
+    },
+    deletePost: function (index) {
+      this.$emit("deletePost", index);
+    },
+  },
+  template: `
+  <div class="bg-primary p-2 rounded-2 my-2">
+    <h4>{{title}}</h4>
+    <p class="content rounded-2 p-2">{{content}}</p>
+    <p class="m-0">Author: {{author}}</p>
+    <p class="m-0">Creation Date: {{creationDate}}</p>
+    <p class="m-0">Image: {{image}}</p>
+    <div>
+      <input type="button" value="Edit" @click="editPost(index)" class="mx-1 p-1 rounded-1">
+      <input type="button" value="Delete" @click="deletePost(index)" class="mx-1 p-1 rounded-1">
+    </div>
+  </div>`,
+};
